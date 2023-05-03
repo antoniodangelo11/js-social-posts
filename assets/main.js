002c90ffca0d990b4a3fa06477479ff16e0f7c2f
antoninodangelo11 <reapercsp@gmail.com>
2023-05-03 17:37:02 +0200

    add click button

--- assets/main.js
+++ assets/main.js
@@ -55,3 +55,59 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const elePost = document.querySelector('.posts-list');
+
+
+posts.forEach((element) => {
+  elePost.innerHTML += `<div class="post">
+<div class="post__header">
+    <div class="post-meta">                    
+        <div class="post-meta__icon">
+            <img class="profile-pic" src="${element.author.image}" alt="LF">                    
+        </div> 
+         <div class="post-meta__data">
+            <div class="post-meta__author">${element.author.name}</div>
+            <div class="post-meta__time"${element.created}</div>
+        </div>                    
+    </div>
+</div>
+<div class="post__text">${element.content}</div>
+<div class="post__image">
+    <img src="${element.media}" alt="">
+</div>
+<div class="post__footer">
+    <div class="likes js-likes">
+        <div class="likes__cta">
+            <button class="like-button  js-like-button" data-postid="${element.id}">
+                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                <span class="like-button__label">Mi Piace</span>
+            </button>
+        </div>
+        <div class="likes__counter">
+            Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
+        </div>
+    </div> 
+</div>            
+</div>`
+});
+
+const eleLikeButtons = document.querySelectorAll(".like-button");
+const eleCounters = document.querySelectorAll(".js-likes-counter")
+
+for (let i = 0; i < eleLikeButtons.length; i++) {
+  const eleLike = eleLikeButtons[i];
+
+  eleLike.addEventListener("click", function () {
+    const eleCounter = eleCounters[i];
+
+    if (eleLike.classList.contains("like-button--liked")) {
+      posts[i].likes -= 1;
+    } else {
+      posts[i].likes += 1;
+    }
+
+    eleLike.classList.toggle("like-button--liked");
+    eleCounter.innerHTML = `${posts[i].likes}`;
+  });
+}
